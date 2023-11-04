94e793ee0837eddde99b9505323538e559f0d537
george <georget415@gmail.com>
2023-11-04 15:21:51 +0000

    * added routeset functionality to generate multiple routes from a start point

--- main.py
+++ main.py
@@ -1,55 +1,106 @@
+import os
+import uuid
+import json
 import requests
 import folium
+import matplotlib.pyplot as plt
+import contextily as ctx
 from collections import namedtuple
+import math
+import random
 
-# Define a named tuple for coordinates
+BASEMAPS = {
+    "OpenStreetMap": ctx.providers.OpenStreetMap.Mapnik,
+    "GoogleSatellite": "https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+}
+
 Coordinate = namedtuple('Coordinate', ['longitude', 'latitude'])
 
-# Utility function to convert (Long, Lat) to (Lat, Long) for folium
-def to_lat_long(coord: Coordinate) -> tuple:
-    return (coord.latitude, coord.longitude)
 
-def fetch_osrm_route(start: Coordinate, end: Coordinate, osrm_url: str = 'http://localhost:5000') -> dict:
-    # Use (Long, Lat) order directly as it's the default
-    coordinates = f"{start.longitude},{start.latitude};{end.longitude},{end.latitude}"
-    route_url = f"{osrm_url}/route/v1/walking/{coordinates}?overview=full&geometries=geojson"
+class RouteSet:
 
-    response = requests.get(route_url)
-    if response.status_code != 200:
-        raise ConnectionError(f"Failed to fetch the route from OSRM. HTTP Status Code: {response.status_code}.")
+    def __init__(self, start: Coordinate, distance: float, num_routes: int):
+        self.start = start
+        self.distance = distance
+        self.num_routes = num_routes
+        self.routeset_id = f"routeset_{str(uuid.uuid4())[:8]}"
+        self.routeset_directory = os.path.join('routesets', self.routeset_id)
+        os.makedirs(self.routeset_directory, exist_ok=True)
 
-    return response.json()
+    def generate_end_points(self):
+        delta_lat = self.distance / 111.0
+        delta_long = self.distance / (111.0 * math.cos(math.radians(self.start.latitude)))
+        angle = 2 * math.pi * random.random()
+        d_lat = delta_lat * math.sin(angle)
+        d_long = delta_long * math.cos(angle)
+        new_lat = self.start.latitude + d_lat
+        new_long = self.start.longitude + d_long
+        return Coordinate(longitude=new_long, latitude=new_lat)
 
-def plot_route_on_map(route: dict, start: Coordinate, end: Coordinate) -> folium.Map:
-    # Convert OSRM's (Long, Lat) coordinates to Folium's (Lat, Long) format
-    route_coords = [to_lat_long(Coordinate(longitude=coord[0], latitude=coord[1])) for coord in route['routes'][0]['geometry']['coordinates']]
+    def generate_routes(self):
+        for _ in range(self.num_routes):
+            end = self.generate_end_points()
+            route = self.fetch_osrm_route(self.start, end)
+            route_id = self.save_route(route)
+            self.visualize_route(route, end, route_id)
 
-    # Initialize a folium map centered on the starting point of the route
-    route_map = folium.Map(location=to_lat_long(start), zoom_start=14)
+    @staticmethod
+    def to_lat_long(coord: Coordinate) -> tuple:
+        return (coord.latitude, coord.longitude)
 
-    # Add the route line to the map
-    folium.PolyLine(
-        locations=route_coords,  # Coordinates are now in (Lat, Long) format suitable for folium
-        color='blue',
-        weight=5,
-        opacity=0.7
-    ).add_to(route_map)
+    @staticmethod
+    def fetch_osrm_route(start: Coordinate, end: Coordinate, osrm_url: str = 'http://localhost:5000') -> dict:
+        coordinates = f"{start.longitude},{start.latitude};{end.longitude},{end.latitude}"
+        route_url = f"{osrm_url}/route/v1/walking/{coordinates}?overview=full&geometries=geojson"
+        response = requests.get(route_url)
+        if response.status_code != 200:
+            raise ConnectionError(f"Failed to fetch the route from OSRM. HTTP Status Code: {response.status_code}.")
+        return response.json()
 
-    # Add markers for start and end points
-    folium.Marker(location=to_lat_long(start), popup='Start', icon=folium.Icon(color='green')).add_to(route_map)
-    folium.Marker(location=to_lat_long(end), popup='End', icon=folium.Icon(color='red')).add_to(route_map)
+    def save_route(self, route: dict) -> str:
+        route_uuid = str(uuid.uuid4())[:8]
+        route_directory = os.path.join(self.routeset_directory, f"route_{route_uuid}")
+        os.makedirs(route_directory, exist_ok=True)
+        with open(os.path.join(route_directory, 'route.geojson'), 'w') as f:
+            json.dump(route, f)
+        return route_uuid
 
-    return route_map
+    def visualize_route(self, route: dict, end: Coordinate, route_id: str):
+        self.plot_route_on_map(route, self.start, end, route_id)
+        selected_basemap = BASEMAPS["OpenStreetMap"]
+        self.plot_route_on_map_mpl(route, self.start, end, selected_basemap, route_id)
 
-# Set start and end locations using (Long, Lat) order by default
-start_location = Coordinate(longitude=-4.4824, latitude=54.1663)
-end_location = Coordinate(longitude=-4.4938, latitude=54.1562)
+    def plot_route_on_map(self, route: dict, start: Coordinate, end: Coordinate, route_id: str) -> folium.Map:
+        route_coords = [RouteSet.to_lat_long(Coordinate(longitude=coord[0], latitude=coord[1]))
+                        for coord in route['routes'][0]['geometry']['coordinates']]
+        route_map = folium.Map(location=RouteSet.to_lat_long(start), zoom_start=14)
+        folium.TileLayer(tiles=BASEMAPS["OpenStreetMap"]).add_to(route_map)
+        folium.PolyLine(route_coords, color='blue', weight=5, opacity=0.7).add_to(route_map)
+        folium.Marker(location=RouteSet.to_lat_long(start), popup='Start', icon=folium.Icon(color='green')).add_to(route_map)
+        folium.Marker(location=RouteSet.to_lat_long(end), popup='End', icon=folium.Icon(color='red')).add_to(route_map)
+        route_directory = os.path.join(self.routeset_directory, f"route_{route_id}")
+        route_map.save(os.path.join(route_directory, "route_map.html"))
+        return route_map
 
-# Fetch the route data using the specified start and end points
-route_data = fetch_osrm_route(start_location, end_location)
+    def plot_route_on_map_mpl(self, route: dict, start: Coordinate, end: Coordinate, basemap_style: str, route_id: str) -> None:
+        fig, ax = plt.subplots(figsize=(8, 8))
+        x = [coord[0] for coord in route['routes'][0]['geometry']['coordinates']]
+        y = [coord[1] for coord in route['routes'][0]['geometry']['coordinates']]
+        ax.plot(x, y, color='blue', linewidth=2)
+        ax.scatter(start.longitude, start.latitude, color='green', label='Start')
+        ax.scatter(end.longitude, end.latitude, color='red', label='End')
+        ax.axis('off')
+        buffer = 0.001
+        ax.set_xlim(min(x) - buffer, max(x) + buffer)
+        ax.set_ylim(min(y) - buffer, max(y) + buffer)
+        ctx.add_basemap(ax, crs='EPSG:4326', source=basemap_style)
+        plt.tight_layout()
+        route_directory = os.path.join(self.routeset_directory, f"route_{route_id}")
+        plt.savefig(os.path.join(route_directory, 'route_map_mpl.png'), dpi=150)
+        plt.show()
 
-# Plot the fetched route on a folium map
-map_with_route = plot_route_on_map(route_data, start_location, end_location)
 
-# Save the generated map visualization to an HTML file
-map_with_route.save("route_map.html")
+if __name__ == "__main__":
+    start_location = Coordinate(longitude=-4.4824, latitude=54.1663)
+    route_set = RouteSet(start=start_location, distance=5.0, num_routes=3)
+    route_set.generate_routes()
